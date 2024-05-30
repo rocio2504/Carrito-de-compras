7e4c462be103d34e43efdb95ec39d2ebc720195d
rocio2504 <rocioandrea250489@gmail.com>
2024-05-30 09:57:19 -0500

    Base de Datos

--- BD/BD-Carrito-de-compras.sql
+++ BD/BD-Carrito-de-compras.sql
@@ -20,10 +20,11 @@ CREATE TABLE MARCA(
 IdMarca INT PRIMARY KEY IDENTITY(1,1),
 Descripcion VARCHAR(100),
 Activo BIT DEFAULT 1,
-FechaRegisto DATETIME DAFAULT GETDATE()
+FechaRegisto DATETIME DEFAULT GETDATE()
 )
 GO
-
+-- ROCIO MAMANI
+-- -----------------------------------------
 CREATE TABLE PRODUCTO(
 IdProducto INT PRIMARY KEY IDENTITY(1,1),
 Nombre VARCHAR(500),
@@ -35,17 +36,19 @@ Stock INT,
 RutaImagen VARCHAR(100),
 NombreImagen VARCHAR(100),
 Activo BIT DEFAULT 1,
-FechaRegisto DATETIME DAFAULT GETDATE()
+FechaRegisto DATETIME DEFAULT GETDATE()
 )
 
-
+-- ROCIO MAMANI
+-- -----------------------------------------
 -- Creacion de FK PRODUCTO
 ALTER TABLE PRODUCTO
 ADD CONSTRAINT Fk_PRODUCTO_MARCA
 FOREIGN KEY (IdMarca) 
 REFERENCES MARCA (IdMarca)
 GO
-
+-- ROCIO MAMANI
+-- -----------------------------------------
 ALTER TABLE PRODUCTO
 ADD CONSTRAINT Fk_PRODUCTO_CATEGORIA
 FOREIGN KEY (IdCategoria) 
@@ -61,14 +64,16 @@ Clave VARCHAR (150),
 Reestablecer BIT DEFAULT 0, 
 FechaRegistro DATETIME DEFAULT GETDATE()
 )
-
+-- ROCIO MAMANI
+-- -----------------------------------------
 CREATE TABLE CARRITO (
 Idcarrito INT PRIMARY KEY IDENTITY(1,1),
 IdCliente INT,
 IdProducto INT,
 Cantidad INT
 )
-
+-- ROCIO MAMANI
+-- -----------------------------------------
 -- Creacion de FK CARRITO
 ALTER TABLE CARRITO
 ADD CONSTRAINT Fk_CARRITO_CLIENTE
@@ -81,7 +86,8 @@ ADD CONSTRAINT Fk_CARRITO_PRODUCTO
 FOREIGN KEY (IdProducto) 
 REFERENCES PRODUCTO (IdProducto)
 GO
-
+-- ROCIO MAMANI
+-- -----------------------------------------
 CREATE TABLE VENTA(
 IdVenta INT PRIMARY KEY IDENTITY(1,1),
 IdCliente INT,
@@ -95,7 +101,8 @@ IdTransaccion VARCHAR (50),
 FechaVenta DATETIME DEFAULT GETDATE()
 )
 GO
-
+-- ROCIO MAMANI
+-- -----------------------------------------
 ALTER TABLE VENTA
 ADD CONSTRAINT Fk_VENTA_CLIENTE
 FOREIGN KEY (IdCliente) 
@@ -116,13 +123,15 @@ ADD CONSTRAINT Fk_DETALLE_VENTA_VENTA
 FOREIGN KEY (IdVenta) 
 REFERENCES VENTA (IdVenta)
 GO
-
+-- ROCIO MAMANI
+-- -----------------------------------------
 ALTER TABLE DETALLE_VENTA
 ADD CONSTRAINT Fk_DETALLE_VENTA_PRODUCTO
 FOREIGN KEY (IdProducto) 
 REFERENCES PRODUCTO (IdProducto)
 GO
-
+-- ROCIO MAMANI
+-- -----------------------------------------
 CREATE TABLE USUARIO(
 IdUsuario INT PRIMARY KEY IDENTITY(1,1),
 Nombres VARCHAR (100), 
@@ -135,10 +144,45 @@ FechaRegistro DATETIME DEFAULT GETDATE()
 )
 GO
 
-CREATE TABLE MARCA(
-IdMarca INT PRIMARY KEY IDENTITY(1,1),
-Descripcion VARCHAR (100), 
-Activo BIT DEFAULT 1,
-FechaRegistro DATETIME DEFAULT GETDATE()
+CREATE TABLE DEPARTAMENTO(
+IdDepartamento VARCHAR(2) PRIMARY KEY,
+Descripcion VARCHAR(45) NOT NULL
 )
 GO
+-- ROCIO MAMANI
+-- -----------------------------------------
+CREATE TABLE PROVINCIA(
+IdProvincia VARCHAR(4) NOT NULL PRIMARY KEY,
+Descripcion VARCHAR(45) NOT NULL,
+IdDepartamento VARCHAR(2)
+)
+GO
+
+ALTER TABLE PROVINCIA
+ADD CONSTRAINT Fk_PROVINCIA_DEPARTAMENTO 
+FOREIGN KEY (IdDepartamento) 
+REFERENCES DEPARTAMENTO(IdDepartamento)
+GO
+
+CREATE TABLE DISTRITO(
+IdDistrito VARCHAR(6) NOT NULL PRIMARY KEY,
+Descripcion VARCHAR(45) NOT NULL,
+IdProvincia VARCHAR(4) NOT NULL,
+IdDepartamento VARCHAR(2) NOT NULL
+)
+
+ALTER TABLE DISTRITO
+ADD CONSTRAINT Fk_DISTRITO_PROVINCIA
+FOREIGN KEY (IdProvincia) 
+REFERENCES PROVINCIA (IdProvincia)
+GO
+-- ROCIO MAMANI
+-- -----------------------------------------
+ALTER TABLE DISTRITO
+ADD CONSTRAINT Fk_DISTRITO_DEPARTAMENTO
+FOREIGN KEY (IdDepartamento) 
+REFERENCES DEPARTAMENTO(IdDepartamento)
+GO
+
+
+
