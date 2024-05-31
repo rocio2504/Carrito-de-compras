4d6120f0a791d4415c3887cfe418d2c2827bcdb4
rocio2504 <rocioandrea250489@gmail.com>
2024-05-30 23:45:28 -0500

    update insert BD & update code

--- AppWeb/AppWeb/Controllers/HomeController.cs
+++ AppWeb/AppWeb/Controllers/HomeController.cs
@@ -28,5 +28,10 @@ namespace AppWeb.Controllers
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
+        public IActionResult PaginaTest()
+        {
+            return View();
+        }
+
     }
 }
