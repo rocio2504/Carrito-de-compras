c474cc8368fbb0fa0d6e4b1dae1e75880b864933
rocio2504 <rocioandrea250489@gmail.com>
2024-06-01 00:33:57 -0500

    updating

---
+++ AppWeb/CapaEntidad/Categoria.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Principal;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CapaEntidad
+{
+    public class Categoria
+    {
+   //     CREATE TABLE CATEGORIA(
+   // IdCategoria INT PRIMARY KEY IDENTITY (1,1),
+  //  Descripcion VARCHAR(100),
+  //  Activo BIT DEFAULT 1,
+   // FechaRegistro DATETIME DEFAULT GETDATE()
+    //)
+        public int IdCategoria { get; set; }
+
+
+    }
+}
